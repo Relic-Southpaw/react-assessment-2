05944ef4589e30ab2f6540e111ed5d21178e1c91
Relic <RobertSmithTheFourth@Outlook.com>
2023-09-19 20:15:14 -0400

    made unroll function

---
+++ unroll/unroll.js
@@ -0,0 +1,16 @@
+const unroll = (squareArray) => {
+    const arr = [];
+
+    while (squareArray.length) {
+        arr.push(
+            ...squareArray.shift(),
+            ...squareArray.map(a => a.pop()),
+            ...(squareArray.pop() || []).reverse(),
+            ...squareArray.map(a => a.shift()).reverse()
+        );
+    }
+    return arr;
+}
+
+
+module.exports = unroll;
